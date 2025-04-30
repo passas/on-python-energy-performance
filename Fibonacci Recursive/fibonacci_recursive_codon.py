aa9913f956c91d97ed81388d10901c2ca5b72655
Sérgio <passinhas325@gmail.com>
2025-04-30 02:28:09 +0100

    Fib

---
+++ Fibonacci Recursive/fibonacci_recursive_codon.py
@@ -0,0 +1,21 @@
+import sys
+
+def fib (n):
+   if n == 0:
+      return 0
+   if n == 1:
+      return 1
+   return fib(n-1) + fib(n-2)
+   
+def main():
+   if len(sys.argv) != 2:
+      print("Usage: " + sys.argv[0] + " <n>")
+      return
+   n = int(sys.argv[1])
+   if (n<0):
+      print("Value of n must be non-negative.")
+   result = fib( n )
+   print ('Fibonacci('+str(n)+') = ('+str(result)+')')
+
+if __name__ == '__main__':
+   main()
